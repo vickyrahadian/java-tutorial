6cbdca1302099e93e86f62f89700705910c20a29
Vicky <vicky.rahadian@gmail.com>
2022-06-01 22:24:02 +0700

    init

---
+++ spring/eazybytes/ws/spring-main/example_35/src/main/java/com/eazybytes/eazyschool/model/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eazyschool.model;
+
+import lombok.Data;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime updatedAt;
+    private String updatedBy;
+}
